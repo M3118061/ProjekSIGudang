aafdf035b25a14c86e86334061c357f5775ad037
M3118061 <62411823+M3118061@users.noreply.github.com>
2021-06-17 08:47:51 +0700

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,6 +6,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
     </ul>
+    @if(Auth::check())
     <ul class="navbar-nav ml-auto">
       <!-- Telah Expired -->
       <li class="nav-item dropdown">
@@ -15,7 +16,7 @@
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
           <span class="dropdown-item dropdown-header">PERHATIAN!</span>
-          <a href="" class="dropdown-item">
+          <a href="{{ route('products') }}?status=1" class="dropdown-item">
             Ada <span id="notif_expired" class="font-weight-bold">0</span> produk telah expired!
           </a>
         </div>
@@ -29,41 +30,18 @@
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
           <span class="dropdown-item dropdown-header">PERHATIAN!</span>
-          <a href="" class="dropdown-item">
+          <a href="{{ route('products') }}?status=2" class="dropdown-item">
             Ada <span id="notif_reminder" class="font-weight-bold">0</span> produk akan segera expired!
           </a>
         </div>
       </li>
-
+      @endif
       <!-- Home -->
       <li class="nav-item d-none d-sm-inline-block">
         <a href="{{ url('/') }}" class="nav-link">
           <i class="fas fa-home"></i>
         </a>
       </li>
-      
-      {{--  @php
-          $stokBarang = \DB::select("SELECT * FROM stok_barang where tgl_exp < created_at");
-      @endphp
-
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">Barang EXP</span>
-          <div class="dropdown-divider"></div>
-          @foreach ($stokBarang as $stokBarang)
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i>{{ $stokBarang->nama_barang }}
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          @endforeach
-        </div>
-      </li>  --}}
-
       <li class="nav-item">
         <a href="{{ route('logout') }}"
             onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
@@ -74,5 +52,41 @@
         </form>
       </li>
     </ul>
+
+    @if(Auth::check())
+    <script>
+      $(function () {
+          $.ajaxSetup({
+              headers: {
+                  'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+              }
+          });
+          expiredNotif();
+          setInterval(expiredNotif, 10000);
+      });
+      function expiredNotif(){
+          $.ajax({
+              url: "{{ route('products.expiryCheck') }}",
+              type: "GET",
+              data: {"format": "json"},
+              dataType: "json",
+              success:function(data) {
+                  $('#notif_expired_badge').text(data.count);
+                  $('#notif_expired').text(data.count);
+              }
+          });
+          $.ajax({
+              url: "{{ route('products.expiryCheck') }}",
+              type: "GET",
+              data: {"format": "json", "interval":true},
+              dataType: "json",
+              success:function(data) {
+                  $('#notif_reminder_badge').text(data.count);
+                  $('#notif_reminder').text(data.count);
+              }
+          });
+      }
+    </script>
+    @endif
   </nav>
   <!-- /.navbar -->
