68fbe98a073c4b0c9788f8d274731d6148fdc19d
M3118061 <62411823+M3118061@users.noreply.github.com>
2021-06-17 08:45:14 +0700

    Update CheckRole.php

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -4,6 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
 
 class CheckRole
 {
@@ -14,11 +17,22 @@ class CheckRole
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, $role)
+    // public function handle(Request $request, Closure $next, $role)
+    // {
+    //     if($request->user()->role == $role){
+    //         return $next($request);
+    //     }
+    //     return redirect('/');
+    // }
+
+    public function handle(Request $request, Closure $next)
     {
-        if($request->user()->role == $role){
-            return $next($request);
+        if(Auth::check()){
+            if(Auth::user()->role == 0){
+                return $next($request);
+            }
+        } else {
+            return redirect('/');
         }
-        return redirect('/');
     }
 }
